134aaf18f85faa8992359c4f718315982d849d92
Faizalfadaa <faizalfadha@gmail.com>
2026-05-02 02:58:14 +0700

    feat: added fucntions to parser.hpp

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -17,16 +17,47 @@ void Parser::match(TokenType expectedToken) {
 
 }
 
-void Parser::program() {
-    programHeader();
-    declarationPart();
-}
+ParseNode* Parser::program(){}
+ParseNode* Parser::programHeader(){}
+ParseNode* Parser::declarationPart(){}
+ParseNode* Parser::constDeclaration(){}
+ParseNode* Parser::constant(){}
+ParseNode* Parser::typeDeclaration(){}
+ParseNode* Parser::varDeclaration(){}
+ParseNode* Parser::identifierList(){}
+ParseNode* Parser::type(){}
+ParseNode* Parser::arrayType(){}
 
-void Parser::programHeader() {
+ParseNode* Parser::range(){}
+ParseNode* Parser::enumerated(){}
+ParseNode* Parser::recordType(){}
+ParseNode* Parser::fieldList(){}
+ParseNode* Parser::fieldPart(){}
+ParseNode* Parser::subProgramDeclaration(){}
+ParseNode* Parser::procedureDeclaration(){}
+ParseNode* Parser::functionDeclaration(){}
+ParseNode* Parser::block(){}
+ParseNode* Parser::formalParameterList(){}
 
-}
+ParseNode* Parser::parameterGroup(){}
+ParseNode* Parser::compoundStatement(){}
+ParseNode* Parser::statementList(){}
+ParseNode* Parser::statement(){}
+ParseNode* Parser::assignmentStatement(){}
+ParseNode* Parser::ifStatement(){}
+ParseNode* Parser::caseStatement(){}
+ParseNode* Parser::caseBlock(){}
+ParseNode* Parser::whileStatement(){}
+ParseNode* Parser::repeatStatement(){}
 
-void Parser::declarationPart() {
-
-}
+ParseNode* Parser::forStatement(){}
+ParseNode* Parser::procedureFunctionCall(){}
+ParseNode* Parser::parameterList(){}
+ParseNode* Parser::expression(){}
+ParseNode* Parser::simpleExpression(){}
+ParseNode* Parser::term(){}
+ParseNode* Parser::factor(){}
+ParseNode* Parser::relationalOperator(){}
+ParseNode* Parser::additiveOperator(){}
+ParseNode* Parser::multiplicativeOperator(){}
 
--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -14,11 +14,51 @@ class Parser {
         TokenType currToken;
         std::string currLexeme;
 
-        void match(TokenType expectedToken);
-        void program();
-        void programHeader();
-        void declarationPart();
+        ParseNode* match(TokenType expectedToken);
 
+        ParseNode* program();
+        ParseNode* programHeader();
+        ParseNode* declarationPart();
+        ParseNode* constDeclaration();
+        ParseNode* constant();
+        ParseNode* typeDeclaration();
+        ParseNode* varDeclaration();
+        ParseNode* identifierList();
+        ParseNode* type();
+        ParseNode* arrayType();
+        
+        ParseNode* range();
+        ParseNode* enumerated();
+        ParseNode* recordType();
+        ParseNode* fieldList();
+        ParseNode* fieldPart();
+        ParseNode* subProgramDeclaration();
+        ParseNode* procedureDeclaration();
+        ParseNode* functionDeclaration();
+        ParseNode* block();
+        ParseNode* formalParameterList();
+        
+        ParseNode* parameterGroup();
+        ParseNode* compoundStatement();
+        ParseNode* statementList();
+        ParseNode* statement();
+        ParseNode* assignmentStatement();
+        ParseNode* ifStatement();
+        ParseNode* caseStatement();
+        ParseNode* caseBlock();
+        ParseNode* whileStatement();
+        ParseNode* repeatStatement();
+        
+        ParseNode* forStatement();
+        ParseNode* procedureFunctionCall();
+        ParseNode* parameterList();
+        ParseNode* expression();
+        ParseNode* simpleExpression();
+        ParseNode* term();
+        ParseNode* factor();
+        ParseNode* relationalOperator();
+        ParseNode* additiveOperator();
+        ParseNode* multiplicativeOperator();
 };
 
 
